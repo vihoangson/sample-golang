4d69b3ea1303aff23db110e651374f947ccc2fcb
lampart_vi_hoang_son <vihoangson@gmail.com>
2019-12-19 00:21:35 +0700

    mysql

--- gin/sample1/main.go
+++ gin/sample1/main.go
@@ -1,19 +1,54 @@
 package main
-import (
 
-"fmt"
-"github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	router := gin.Default()
+	// go get -u github.com/go-sql-driver/mysql
+	db, err := sql.Open("mysql", "root:@/golang")
+	if err != nil {
+		panic(err.Error())  // Just for example purpose. You should use proper error handling instead of panic
+	}
+	defer db.Close()
 
-	router.POST("/post", func(c *gin.Context) {
+	// Prepare statement for inserting data
+	stmtIns, err := db.Prepare("INSERT INTO squareNum VALUES( ?, ? )") // ? = placeholder
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
 
-		ids := c.QueryMap("ids")
-		names := c.PostFormMap("names")
+	// Prepare statement for reading data
+	stmtOut, err := db.Prepare("SELECT squareNumber FROM squarenum WHERE number = ?")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer stmtOut.Close()
 
-		fmt.Printf("ids33344: %v; names: %v", ids, names)
-	})
-	router.Run(":8080")
+	// Insert square numbers for 0-24 in the database
+	for i := 0; i < 25; i++ {
+		_, err = stmtIns.Exec(i, (i * i)) // Insert tuples (i, i^2)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+	}
+
+	var squareNum int // we "scan" the result in here
+
+	// Query the square-number of 13
+	err = stmtOut.QueryRow(13).Scan(&squareNum) // WHERE number = 13
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	fmt.Printf("The square number of 13 is: %d", squareNum)
+
+	// Query another number.. 1 maybe?
+	err = stmtOut.QueryRow(1).Scan(&squareNum) // WHERE number = 1
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	fmt.Printf("The square number of 1 is: %d", squareNum)
 }
